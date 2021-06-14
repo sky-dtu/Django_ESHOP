0777ed0f26b93c44ef176cdfe2893ad0e9e22949
Sandeep Yadav <53916295+sky-dtu@users.noreply.github.com>
2021-06-14 13:35:51 +0530

    Modified Allowed Hosts

--- eShop/settings.py
+++ eShop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$99yzf*g-0db7*m+m%v#x(-@l4+0ch1g5a5vgi7dd=_l#x!ac!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [‘eshop-sky.herokuapp.com’]
+ALLOWED_HOSTS = []
 
 
 # Application definition
